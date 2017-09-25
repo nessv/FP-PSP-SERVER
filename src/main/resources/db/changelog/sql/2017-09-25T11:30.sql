e43af87ef7c791320539a7d6d782d6a2341473e3
Nestor Valdez <ness_valdez@hotmail.com>
2017-09-25 09:48:53 -0400

    Revert last commit
    This reverts commit 8a26b1f5a2658455b2909762acc716e926c96320.

--- src/main/resources/db/changelog/sql/2017-09-25T11:30.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE survey.task_tracking
-    RENAME TO intervention_tracking;
-
-ALTER TABLE survey.intervention_tracking
-    RENAME task_tracking_id TO intervention_tracking_id;
-
-ALTER TABLE survey.intervention_tracking
-    RENAME task_id TO intervention_id;
